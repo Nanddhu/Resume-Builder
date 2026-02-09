8f097e5d85e543c7f1dcc44f481046bd4a344745
Nanddhu <nandhinivallal@gmail.com>
2026-02-09 18:13:09 +0530

    stepper and models created

---
+++ ResumeBuilders/Models/Entities/Stepper/ResumeSkill.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResumeBuilders.Models.Entities.Stepper
+{
+    [Table("resumeskills")]
+    public class ResumeSkill
+    {
+        [Key]
+        [Column("ID")]
+        public int ID { get; set; }
+
+        [Column("ResumeID")]
+        public int ResumeID { get; set; }
+
+        [Column("SkillID")]
+        public int SkillID { get; set; }
+
+        [Column("CreatedAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("UpdatedAt")]
+        public DateTime UpdatedAt { get; set; }
+
+        [Column("SkillName")]
+        public string SkillName { get; set; }
+    }
+}
